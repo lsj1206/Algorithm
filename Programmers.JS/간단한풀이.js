f1ae1c9f6ab4092264899486bf1b82c8457f1215
LeeSJ0516 <1007ckddjs12@gmail.com>
2024-07-23 18:45:16 +0900

    add

--- Programmers.JS/간단한풀이.js
+++ Programmers.JS/간단한풀이.js
@@ -1,3 +1,15 @@
+
+//2016년
+//https://school.programmers.co.kr/learn/courses/30/lessons/12901
+
+function getDate(a, b) {
+	var Day = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+	var date = new Date(2016, a - 1, b);
+
+	return Day[date.getDay()];
+}
+//console.log(getDate(5, 24));
+
 // 음양 더하기
 // https://school.programmers.co.kr/learn/courses/30/lessons/76501
 
@@ -5,8 +17,7 @@ function signCheckSum(absolutes, signs) {
 	var anwser = absolutes.map((num, index) => signs[index] ? num : num * -1);
 	return (anwser.reduce((acc, cur) => acc + cur));
 }
-
-console.log(signCheckSum([4,7,12], [true,false,true]));
+//console.log(signCheckSum([4,7,12], [true,false,true]));
 
 // 없는 숫자 더하기
 //https://school.programmers.co.kr/learn/courses/30/lessons/86051
@@ -17,5 +28,4 @@ function missingNumSum(numbers) {
 
 	return (anwser.reduce((acc, cur) => acc + cur));
 }
-
-console.log(missingNumSum([1,2,3,4,6,7,8,0]));
+//console.log(missingNumSum([1,2,3,4,6,7,8,0]));
